2e88a722a9799ee8bb6d67427c0d6d3f6951328b
Alexey29 <alexrudyk@mail.ru>
2017-04-27 23:58:40 +0300

    add mainPage.fxml and MainPageController

--- src/main/java/com/opu/database/Controllers/fxmlController/MainPageController.java
+++ src/main/java/com/opu/database/Controllers/fxmlController/MainPageController.java
@@ -1,7 +1,20 @@
 package com.opu.database.Controllers.fxmlController;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 /**
  * Created by antipavitaly on 4/27/17.
  */
 public class MainPageController {
+    private static int i = 1;
+
+    @FXML
+    private Label nNotes;
+    public void onClick(MouseEvent mouseEvent) {
+
+        nNotes.setText(i+" заметок");
+
+    }
 }
