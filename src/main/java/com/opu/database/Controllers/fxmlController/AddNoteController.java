b389e60793a509e8800c4f7d35777ba3faf88a24
Vitaly <meloman787@gmail.com>
2017-05-24 21:38:39 +0300

    Refactoring and recommit

--- src/main/java/com/opu/database/Controllers/fxmlController/AddNoteController.java
+++ src/main/java/com/opu/database/Controllers/fxmlController/AddNoteController.java
@@ -3,21 +3,23 @@ package com.opu.database.Controllers.fxmlController;
 import com.opu.database.Controllers.EntitiesController;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * Created by antipavitaly on 4/27/17.
  */
 
 public class AddNoteController {
+    @FXML
+    private AnchorPane pane;
 
     private String noteName;
     private String noteCategory;
     private String noteComment;
     private String noteSubnote;
+    private String noteFinalDate;
 
     @FXML
     private Button button;
@@ -34,24 +36,79 @@ public class AddNoteController {
     @FXML
     private TextArea noteCommentField;
 
+    @FXML
+    private DatePicker finalDatePicker;
+
 
 
     @FXML
     public void initialize() {
+
+
+
             button.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
 
                 @Override
                 public void handle(MouseEvent mouseEvent) {
-                    noteName = noteNameField.getText();
-                    noteCategory = noteCategoryField.getText();
-                    noteComment = noteCommentField.getText();
-                    noteSubnote = noteSubnoteField.getText();
 
-                    new EntitiesController().addNote(noteName,noteSubnote,noteCategory,noteComment);
 
+                    if (noteNameField.getText().equals("") || noteCategoryField.getText().equals("") || noteCommentField.getText().equals("") || noteSubnoteField.getText().equals("") || finalDatePicker.getValue() == null) {
+
+                        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+                        dialog.setHeaderText("Error");
+                        dialog.setContentText("Fill all the fields!");
+                        dialog.showAndWait();
+
+
+                    } else {
+
+                        noteName = noteNameField.getText();
+                        noteCategory = noteCategoryField.getText();
+                        noteComment = noteCommentField.getText();
+                        noteSubnote = noteSubnoteField.getText();
+                        noteFinalDate = finalDatePicker.getValue().toString();
+
+                        new EntitiesController().addNote(noteName, noteSubnote, noteFinalDate, noteComment, noteCategory);
+
+                        noteNameField.clear();
+                        noteCategoryField.clear();
+                        noteCommentField.clear();
+                        noteSubnoteField.clear();
+                        finalDatePicker.getEditor().clear();
+
+
+                        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+                        dialog.setHeaderText("Success");
+                        dialog.setContentText("The note was created!");
+                        dialog.showAndWait();
+//                        dialog.setOnCloseRequest(new EventHandler<DialogEvent>() {
+//                            @Override
+//                            public void handle(DialogEvent event) {
+//                                String fxmlFile = "/fxml/mainPage.fxml";
+//                                FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+//                                Parent root = null;
+//                                try {
+//                                    root = loader.load();
+//                                } catch (IOException e) {
+//                                    e.printStackTrace();
+//                                }
+//
+//                                pane.getScene().setRoot(root);
+//                            }
+//                        });
+
+                    }
                 }
             });
-        }
-
     }
+}
+
+
+
+
+
+
+
+
 
--- src/main/java/com/opu/database/Controllers/fxmlController/NotesPageController.java
+++ src/main/java/com/opu/database/Controllers/fxmlController/NotesPageController.java
@@ -1,7 +1,29 @@
 package com.opu.database.Controllers.fxmlController;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
 /**
  * Created by antipavitaly on 5/24/17.
  */
 public class NotesPageController {
+
+    @FXML
+    private VBox categoryBox;
+
+    @FXML
+    private Label notesNum;
+
+    @FXML
+    private ImageView addImage;
+
+    @FXML
+    private VBox addBox;
+
+    @FXML
+    public void initialize (){
+
+    }
 }
